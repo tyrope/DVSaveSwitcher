82e35f219807949a2b2b03e6dc1bf46871d0bd85
Tyrope <tyrope@tyrope.nl>
2021-02-07 04:52:53 +0100

    Adding some debug printing to hopefully help with #3

--- DVSaveSwitcher.py
+++ DVSaveSwitcher.py
@@ -195,8 +195,12 @@ def process_exists(process_name):
     call = 'TASKLIST','/FI','imagename eq %s' % process_name
     # use buildin check_output right away
     output = subprocess.check_output(call).decode(errors="ignore")
+    print('---- DEBUG BLOCK ----')
+    print(output)
     # check in last line for process name
     last_line = output.strip().split('\r\n')[-1]
+    print(last_line)
+    print('---- DEBUG ENDS  ----')
     # because Fail messages could be translated
     return last_line.lower().startswith(process_name.lower())
 
